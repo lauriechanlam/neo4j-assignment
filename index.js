ec296e0e585c3bf2d8adfd4440cef6c882cbe86d
Laurie Chan Lam <laurie@lauriechanlam.com>
2022-10-04 19:48:25 +0200

    read pyramid from string

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+const makePyramid = (str) => {
+  const layers = str.split('\n')
+  if (layers.length === 0) {
+    throw new Error('pyramid should be initialized with at least 1 line, got 0')
+  }
+  const layerCount = parseInt(layers[0])
+  const pyramid = layers.slice(1).map((layer) => layer.split(' ').map((friction) => parseInt(friction)))
+  if (layers.length - 1 !== layerCount) {
+    throw new Error(`actual pyramid layer count (${layers.length - 1}) should be match declared layer count (${layerCount})`)
+  }
+
+  if (!isValidPyramid(pyramid)) {
+    throw new Error('pyramid should be initialized with a valid pyramid')
+  }
+  return pyramid
+}
+
+const isValidPyramid = (pyramid) => {
+  try {
+    return pyramid.every((layer, index) => layer.length === index + 1)
+  } catch {
+    return false
+  }
+}
+
+module.exports = { makePyramid }
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-test('', () => { })
+const { makePyramid } = require('./index')
+
+describe('read pyramid from string', () => {
+  test('should throw for invalid string', () => {
+    expect(() => makePyramid('')).toThrow()
+    expect(() => makePyramid('abc\ndef')).toThrow()
+    expect(() => makePyramid('0\n')).toThrow()
+    expect(() => makePyramid('1\n1 2')).toThrow()
+  })
+  test('should create a valid pyramid for a valid string', () => {
+    expect(makePyramid('1\n4')).toMatchObject([[4]])
+    expect(makePyramid('2\n5\n10 6')).toMatchObject([[5], [10, 6]])
+  })
+})
